187178b7eb23f41c254c8a845463ba488f879ef0
juditlehoczki <lehoczki.judit@gmail.com>
2020-02-24 16:22:49 +0000

    comments deleted

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -23,16 +23,11 @@ exports.formatComments = (comments, articleRef) => {
     formattedComments.push({ ...comment });
   });
   formattedComments.forEach(comment => {
-    // Its created_by property renamed to an author key
     comment.author = comment.created_by;
     delete comment.created_by;
-    // Its belongs_to property renamed to an article_id key
-    // The value of the new article_id key must be the id corresponding to the original title value provided
     comment.article_id = articleRef[comment.belongs_to];
     delete comment.belongs_to;
-    // Its created_at value converted into a javascript date object
     comment.created_at = new Date(comment.created_at);
-    // The rest of the comment's properties must be maintained
   });
   return formattedComments;
 };
