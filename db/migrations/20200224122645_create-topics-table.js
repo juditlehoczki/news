7513e49740ca8fb9f9d62aae70979299742b37b5
juditlehoczki <lehoczki.judit@gmail.com>
2020-02-24 12:33:37 +0000

    updated topics table's headernames

--- db/migrations/20200224122645_create-topics-table.js
+++ db/migrations/20200224122645_create-topics-table.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("topics", topicsTable => {
-    topicsTable.string("slug").primary(); //.notNullable();
-    topicsTable.string("description").notNullable();
+    topicsTable.string("topic_slug").primary(); //.notNullable();
+    topicsTable.string("topic_description").notNullable();
   });
 };
 
